e11eb01064203847eaba1a754985cc29f641e7f1
Gedvan Dias <gedvan@gmail.com>
2021-01-19 18:25:41 -0300

    Atualizadas bibliotecas do frontend.

--- webpack.config.js
+++ webpack.config.js
@@ -31,9 +31,7 @@ module.exports = {
         use: [
           'vue-style-loader',
           'css-loader',
-          {
-            loader: 'sass-loader'
-          }
+          'sass-loader'
         ]
       },
       {
