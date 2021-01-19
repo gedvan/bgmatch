c3e65e4779b5ff427946185a2f106082e660e0a3
Gedvan Dias <gedvan@gmail.com>
2021-01-19 17:41:05 -0300

    Atualização de bibliotecas do frontend.

--- frontend/views/Ranking.vue
+++ frontend/views/Ranking.vue
@@ -4,7 +4,7 @@
       <h3>Ranking {{ ano }}</h3>
       <b-dropdown text="Anteriores" right size="sm" class="ml-auto align-self-center">
         <b-dropdown-item to="/ranking" :active="$route.path === '/ranking'">Atual</b-dropdown-item>
-        <b-dropdown-item v-for="anoAnterior in anteriores" :to="'/ranking/' + anoAnterior" :active="$route.path === '/ranking/' + anoAnterior">{{ anoAnterior }}</b-dropdown-item>
+        <b-dropdown-item v-for="anoAnterior in anteriores" :key="anoAnterior" :to="'/ranking/' + anoAnterior" :active="$route.path === '/ranking/' + anoAnterior">{{ anoAnterior }}</b-dropdown-item>
       </b-dropdown>
     </header>
     <div class="trilha-pontos">
